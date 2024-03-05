a31fa4220fe41acded6be0153fd1c3cee5335806
Agnieszka Zachariasiewicz <agnieszka.kanarcia@gmail.com>
2024-03-05 01:32:41 +0100

    fixed price form label

--- src/SetPriceForm.jsx
+++ src/SetPriceForm.jsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form"
 import React from 'react'
 
-export default function SetPriceForm({nameList,handleError, addPrice}) {
+export default function SetPriceForm({selectedNameList,handleError, addPrice}) {
   const { register, handleSubmit } = useForm()
   const onSubmit = function(data){
     if(!/^\d+(?:[,.]\d{1,2})?$/.test(data.price)){
@@ -15,7 +15,7 @@ export default function SetPriceForm({nameList,handleError, addPrice}) {
 
   return (
     <form className='flex flex-col w-1/2 items-start gap-5 my-4'  onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="price">{nameList}</label>
+        <label htmlFor="price">{selectedNameList}</label>
      <input id="price" {...register("price")} required={true} placeholder={'Cena...'} className="py-2.5 pl-4 pr-6 rounded-xl text-gray-700 bg-white border border-gray-700" />
       <input type="submit" value="Dodaj cenę" className="bg-black py-2 px-8 text-white border border-black rounded-xl cursor-pointer hover:bg-white hover:text-black"/>
     </form>
